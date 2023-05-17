9407c088d20086b8ef27b5131c5c150bfca17463
Farid Amimri <farid.amimri@gmail.com>
2023-05-17 12:31:21 +0200

    Add wrapper div

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -8,37 +8,35 @@ import Link from 'next/link'
 function Navbar() {
   return (
     <div className={styles.navbar}>
-      <div className={styles.wrapper}>
-        <div className={styles.logo}>
-          <Image src={Logo} alt='logo le créatelier' priority width={120} />
-        </div>
-        <div className={styles.links}>
-          <Link className={styles.link} href='/?category=art'>
-            <h6>ART</h6>
+      <div className={styles.logo}>
+        <Image src={Logo} alt='logo le créatelier' priority width={120} />
+      </div>
+      <div className={styles.links}>
+        <Link className={styles.link} href='/?category=art'>
+          <h6>ART</h6>
+        </Link>
+        <Link className={styles.link} href='/?category=science'>
+          <h6>SCIENCE</h6>
+        </Link>
+        <Link className={styles.link} href='/?category=technology'>
+          <h6>TECHNOLOGY</h6>
+        </Link>
+        <Link className={styles.link} href='/?category=cinema'>
+          <h6>CINEMA</h6>
+        </Link>
+        <Link className={styles.link} href='/?category=design'>
+          <h6>DESIGN</h6>
+        </Link>
+        <Link className={styles.link} href='/?category=food'>
+          <h6>FOOD</h6>
+        </Link>
+        <span>Farid</span>
+        <span>Logout</span>
+        <span className={styles.write}>
+          <Link className={styles.link} href='/write'>
+            Write
           </Link>
-          <Link className={styles.link} href='/?category=science'>
-            <h6>SCIENCE</h6>
-          </Link>
-          <Link className={styles.link} href='/?category=technology'>
-            <h6>TECHNOLOGY</h6>
-          </Link>
-          <Link className={styles.link} href='/?category=cinema'>
-            <h6>CINEMA</h6>
-          </Link>
-          <Link className={styles.link} href='/?category=design'>
-            <h6>DESIGN</h6>
-          </Link>
-          <Link className={styles.link} href='/?category=food'>
-            <h6>FOOD</h6>
-          </Link>
-          <span>Farid</span>
-          <span>Logout</span>
-          <span className={styles.write}>
-            <Link className={styles.link} href='/write'>
-              Write
-            </Link>
-          </span>
-        </div>
+        </span>
       </div>
     </div>
   )
