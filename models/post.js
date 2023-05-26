5714d4804660af68813ca69fbf4821ed98045746
Farid Amimri <farid.amimri@gmail.com>
2023-05-26 10:39:44 +0200

    Post Model Schema

---
+++ models/post.js
@@ -0,0 +1,41 @@
+/** @format */
+
+import { Schema, models, model } from 'mongoose'
+
+const PostSchema = new Schema(
+  {
+    creator: {
+      type: Schema.Types.ObjectId,
+      ref: 'User'
+    },
+    profil: {
+      type: String,
+      required: [true, 'Profil is required']
+    },
+    username: {
+      type: String,
+      required: [true, 'Username is required']
+    },
+    title: {
+      type: String,
+      required: [true, 'Title is required']
+    },
+    description: {
+      type: String,
+      required: [true, 'Description is required']
+    },
+    image: {
+      type: String,
+      required: [true, 'File is required']
+    },
+    category: {
+      type: String,
+      required: [true, 'Category is required']
+    }
+  },
+  { timestamps: true }
+)
+
+const Post = models.Post || model('Post', PostSchema)
+
+export default Post
