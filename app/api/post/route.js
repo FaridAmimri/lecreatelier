66f9b6bb6c72ff821456bce1ae723317f4d526bf
Farid Amimri <farid.amimri@gmail.com>
2023-05-27 19:46:46 +0200

    Handle Fetch by category

--- app/api/post/route.js
+++ app/api/post/route.js
@@ -3,14 +3,24 @@
 import { connectToDB } from '@utils/database'
 import Post from '@models/post'
 
-export const GET = async (req) => {
+export const GET = async (req, res) => {
+  const queryCategory = req.nextUrl.searchParams.get('category')
+  console.log(queryCategory)
+
   try {
     await connectToDB()
 
-    const posts = await Post.find({}).populate('creator')
+    let posts
+    if (queryCategory) {
+      posts = await Post.find({
+        category: queryCategory
+      }).populate('creator')
+    } else {
+      posts = await Post.find({}).populate('creator')
+    }
 
     return new Response(JSON.stringify(posts), { status: 200 })
   } catch (error) {
-    return new Response('Failed to fetch all posts', { status: 500 })
+    return new Response('Failed to fetch posts', { status: 500 })
   }
 }
--- app/page.jsx
+++ app/page.jsx
@@ -5,49 +5,29 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.scss'
 import Link from 'next/link'
 import { useState, useEffect } from 'react'
+import { useSearchParams } from 'next/navigation'
 import axios from 'axios'
-
-// const posts = [
-//   {
-//     id: 1,
-//     title: 'Le Créatelier ? Mais qu’est-ce que c’est ?',
-//     desc: 'Un lieu associatif de travail mais aussi de détente créative ouvert à toutes et tous. Ses activités sont centrées autour de la pratique Illustrative Artistique et Graphique.',
-//     img: 'https://images.pexels.com/photos/7008010/pexels-photo-7008010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
-//   },
-//   {
-//     id: 2,
-//     title: 'Créer pour vivre',
-//     desc: 'On estime que 83% des métiers de 2050 n’existent pas encore. Dans un monde en constante évolution, les métiers de l’art, de la création et du design se diversifient et s’intensifient. Microentrepreneurs, indépendants, petits créateurs, bédéistes et étudiants...autant de profils en pleine expansion qui nécessitent un lieu de pratique, d’échange et de travail.',
-//     img: 'https://images.pexels.com/photos/6489663/pexels-photo-6489663.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
-//   },
-//   {
-//     id: 3,
-//     title: 'Créer pour s’évader',
-//     desc: 'L’avenir est aux esprits agiles ! Lacréativité et l’imaginaire s’unissent pouréveiller les enfants et émerveiller les adultes. Le créatelier a pour vocation de sortir du mythe selon lequel seul l’artiste talentueux à le droit de créer/dessiner et s’évader à travers la pratique manuelle. Le créatelier est un projet de démocratisation de la création: tout le monde peut s’amuser en art ! ',
-//     img: 'https://images.pexels.com/photos/4230630/pexels-photo-4230630.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
-//   },
-//   {
-//     id: 4,
-//     title: 'Créer pour s’inventer',
-//     desc: 'Si les vertus de l’Art-thérapie sont mondialement reconnues, ce n’est pas un hasard ! Non-contente d’être un domaine d’avenir, la création est un véritable outil de revalorisation personnelle, d’introspection et d’évolution. La fierté qui découle d’une création permet d’aller de l’avant, tandis que la découverte des différents processus créatifs aide à s’ouvrir au monde.',
-//     img: 'https://images.pexels.com/photos/6157049/pexels-photo-6157049.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
-//   }
-// ]
 
 export default function Home() {
   const [posts, setPosts] = useState([])
 
+  const searchParams = useSearchParams()
+  const category = searchParams.get('category')
+  console.log(category)
+
   useEffect(() => {
     const fetchPosts = async () => {
       try {
-        const response = await axios.get('/api/post')
+        const response = await axios.get(
+          category ? `/api/post?category=${category}` : '/api/post'
+        )
         setPosts(response.data)
       } catch (error) {
         console.log(error)
       }
     }
     fetchPosts()
-  }, [])
+  }, [category])
 
   const getText = (html) => {
     const doc = new DOMParser().parseFromString(html, 'text/html')
