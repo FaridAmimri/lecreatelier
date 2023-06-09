e01f0631044847945063b32dad263dd20a422a9b
Farid Amimri <farid.amimri@gmail.com>
2023-06-09 09:33:34 +0200

    Add Patch API req

--- app/api/post/[id]/route.js
+++ app/api/post/[id]/route.js
@@ -3,7 +3,7 @@
 import { connectToDB } from '@utils/database'
 import Post from '@models/post'
 
-// GET (read)
+// GET (read) A POST
 export const GET = async (req, { params }) => {
   try {
     await connectToDB()
@@ -17,7 +17,35 @@ export const GET = async (req, { params }) => {
   }
 }
 
-// DELETE
+// UPDATE A POST
+export const PATCH = async (req, { params }) => {
+  const { title, description, image, category } = await req.json()
+
+  try {
+    await connectToDB()
+
+    // Find the existing post by ID
+    const existingPost = await Post.findById(params.id)
+
+    if (!existingPost) {
+      return new Response('Post not found', { status: 404 })
+    }
+
+    // Update the post with new data
+    existingPost.title = title
+    existingPost.description = description
+    existingPost.image = image
+    existingPost.category = category
+
+    await existingPost.save()
+
+    return new Response('Successfully updated the post', { status: 200 })
+  } catch (error) {
+    return new Response('Error updating post', { status: 500 })
+  }
+}
+
+// DELETE A POST
 export const DELETE = async (req, { params }) => {
   try {
     await connectToDB()
